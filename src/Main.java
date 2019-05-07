ff961d72f431d77b605f3685192a55c22055c381
ramikhafagi96 <ramikh.developer@gmail.com>
2019-05-07 16:16:53 +0200

    ReceiveUDP, SendUDP, ListenToTCP

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         MQTTBroker mqttBroker  = MQTTBroker.getInstance();
         mqttBroker.brokerAlwaysOn();
     }
